ef44def79ab5084925573ccb98dd54ea49b7bd0d
bethel-m <bethelcletus87@gmail.com>
2025-01-09 09:13:30 +0100

    add title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Call list site',
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(
               bodyColor: Colors.white,
